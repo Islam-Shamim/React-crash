6808d06b792c00f495cb9fa34735b77f457242ee
Islam-Shamim <islamshamim1819@gmail.com>
2025-02-06 01:08:27 +0600

    onClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const clickHandler = () => {
+    alert('Button Clicked');
+  }
+  const clickHandler2 = () => {
+    alert('Button Clicked 2');
+  }
+  const addToFour = (num) =>{
+    alert(4+num);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Welcome again React</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam corporis officia nesciunt. Quidem nemo, earum ipsa labore perspiciatis in eos! Laborum sequi praesentium ea neque, consequatur molestiae accusamus libero necessitatibus.</p>
       </header>
+      <div className='space-x-2'>
+        <button onClick={clickHandler}>Click Here</button>
+        <button onClick={clickHandler2}>Click Here2</button>
+        <button onClick={()=>alert("No:2, I am here.")}>Click Here3</button>
+        <button onClick={()=>addToFour(3)}>Click Here4</button>
+      </div>
     </div>
   );
 }
